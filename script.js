1cfdda072737f25e793ec2851c8dcb5a7a5fdda6
andregoobloo <177800443+andregoobloo@users.noreply.github.com>
2024-08-21 12:50:09 -0400

    Creates basic  calculator UI

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const display = document.querySelector(".num-display");
+const calculator = document.querySelector(".calculator");
+const num9 = document.querySelector(".nine");
+const num5 = document.querySelector(".five");
+const plus = document.querySelector(".plus");
+const equals = document.querySelector(".equals");
+
 const add = function (num1, num2) {
   return num1 + num2;
 };
@@ -23,4 +30,33 @@ const operate = function (num1, num2, operator) {
   return operator(num1, num2);
 };
 
-console.log(operate(5, 4, divide));
+calculator.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("num")) {
+    display.textContent = e.target.innerText;
+    console.log(e.target.innerText);
+  }
+  if (e.target.classList.contains("clear")) {
+    display.textContent = 0;
+  }
+});
+
+// num9.addEventListener("click", function (e) {
+//   display.textContent = "9";
+//   num1 = 9;
+// });
+// plus.addEventListener("click", function () {
+//   operator = add;
+// });
+// num5.addEventListener("click", function () {
+//   display.textContent = "5";
+//   num2 = 5;
+// });
+// equals.addEventListener("click", function () {
+//   console.log(num1);
+//   console.log(num2);
+//   console.log(operator);
+//   let answer = operate(num1, num2, operator);
+//   console.log(answer);
+//   display.textContent = answer;
+// });
