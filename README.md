b971d4e4f5125ed9c5ea25d78093a63f18ca9ad7
andregoobloo <177800443+andregoobloo@users.noreply.github.com>
2024-08-22 12:52:18 -0400

    Fixes issue with user inputting multiple decimal points in one number

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Odin project 2024
 
 Things to be fixed/added:
 
--User inputting mutliple decimal points.
 -User inputting multiple operators before clicking equal button.
 
 -A smaller output on display to show entire equation.
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const display = document.querySelector(".display");
+const smallDisplay = document.querySelector(".small-display");
 const calculator = document.querySelector(".calculator");
 const numberBtns = document.querySelectorAll(".num");
 const clearBtn = document.querySelector(".clear");
@@ -10,9 +11,13 @@ const multiplyBtn = document.querySelector(".multiply");
 const divideBtn = document.querySelector(".divide");
 const subtractBtn = document.querySelector(".subtract");
 const addBtn = document.querySelector(".add");
+const decimalBtn = document.querySelector(".decimal");
 
 // Variables for calculator operation
-let num1, num2, operator, displayArray;
+let num1,
+  num2,
+  operator,
+  displayArray = [];
 
 // Calculator functions
 const add = function (num1, num2) {
@@ -31,24 +36,38 @@ const divide = function (num1, num2) {
   return num1 / num2;
 };
 
+const operate = function (num1, num2, operator) {
+  return operator(num1, num2);
+};
+
 const equals = function () {
   if (operator === multiply) {
-    displayArray = display.textContent.split(`x`);
+    displayArray = smallDisplay.textContent.split(`x`); //
   } else if (operator === divide) {
-    displayArray = display.textContent.split(`÷`);
+    displayArray = smallDisplay.textContent.split(`÷`);
   } else if (operator === add) {
-    displayArray = display.textContent.split(`+`);
+    displayArray = smallDisplay.textContent.split(`+`);
   } else if (operator === subtract) {
-    displayArray = display.textContent.split(`-`);
+    displayArray = smallDisplay.textContent.split(`-`);
   }
   num1 = Number(displayArray[0]);
   num2 = Number(displayArray[1]);
   let result = operate(num1, num2, operator);
   display.textContent = result;
+  smallDisplay.textContent = result;
 };
 
-const operate = function (num1, num2, operator) {
-  return operator(num1, num2);
+const operaterBtnsFunctionality = function (e) {
+  if (
+    smallDisplay.textContent.includes("+") ||
+    smallDisplay.textContent.includes("-") ||
+    smallDisplay.textContent.includes("x") ||
+    smallDisplay.textContent.includes("÷")
+  ) {
+    equals();
+  }
+  display.textContent = "";
+  smallDisplay.textContent += e.target.innerText;
 };
 
 // Calculator display
@@ -56,25 +75,32 @@ calculator.addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.classList.contains("num")) {
     display.textContent += e.target.innerText;
+    smallDisplay.textContent += e.target.innerText;
   }
   if (e.target.classList.contains("multiply")) {
-    display.textContent += e.target.innerText;
+    operaterBtnsFunctionality(e);
     operator = multiply;
   }
   if (e.target.classList.contains("divide")) {
-    display.textContent += e.target.innerText;
+    operaterBtnsFunctionality(e);
     operator = divide;
   }
   if (e.target.classList.contains("subtract")) {
-    display.textContent += e.target.innerText;
+    operaterBtnsFunctionality(e);
     operator = subtract;
   }
   if (e.target.classList.contains("add")) {
-    display.textContent += e.target.innerText;
+    operaterBtnsFunctionality(e);
     operator = add;
+  }
+  if (e.target.classList.contains("decimal")) {
+    if (display.textContent.includes(".")) return;
+    display.textContent += e.target.innerText;
+    smallDisplay.textContent += e.target.innerText;
   }
   if (e.target.classList.contains("clear")) {
     display.textContent = "";
+    smallDisplay.textContent = "";
   }
   if (e.target.classList.contains("delete")) {
     display.textContent = display.textContent.slice(0, -1);
@@ -83,3 +109,8 @@ calculator.addEventListener("click", function (e) {
     equals();
   }
 });
+
+// Then in the main display it only shows one element of the array at once
+// Makes it easier to stop user from inputting multiple operators
+// multiple operators can be deal with by using array length
+// equal function is run when operand clicked when arraylength === 2
